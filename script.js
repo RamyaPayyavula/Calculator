24ff7c80d775ff639c40698a18730709e40df642
Ramya <ramyaram48@gmail.com>
2019-10-30 22:02:58 -0500

    everything is working now even multiple operators

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let tempstring='';
 let result = 0;
 const res = document.querySelector(".result");
@@ -18,7 +17,6 @@ function performMath(number1,number2,symbol){
             result = number1 + number2;
             break;
     }
-    tempstring = '';
 }
 function reset(){
     tempstring= '';
@@ -26,30 +24,63 @@ function reset(){
 }
 
 function getResult(){
-    const value = result ? result : tempstring ? tempstring : 0;
+    symbols = symbolsAt();
+    const value = result && symbols.length === 0 ? result : tempstring ? tempstring : 0;
     res.innerText = value;
 }
 function goBackOne(){
     const len = tempstring.length
     tempstring =  tempstring.substring(0,len-1);
 }
+
+function symbolsAt(){
+let symbolsAtPosition = [];
+for(let i=0; i<tempstring.length ;i++){
+    if(isNaN(tempstring[i])){
+        symbolsAtPosition=symbolsAtPosition.concat(i);
+    }
+}
+return symbolsAtPosition;
+}
 function processTempString(){
-    let number1;
+    const symbols = symbolsAt();
     let number2;
-    for(let i=0; i<tempstring.length ;i++){
-        if(isNaN(tempstring[i])){
-            number1 = Number(tempstring.substring(0,i));
-            number2 = Number(tempstring.substring(i+1,tempstring.length));
-            performMath(number1, number2,tempstring[i]);
-            break;
+    if(symbols.length === 0){
+        result = tempstring;
+    }
+    else if(symbols.length===1){
+        console.log('coming 1')
+
+        symbolIndex = symbols[0];
+        symbol = tempstring[symbolIndex];
+        const number1 = Number(tempstring.substring(0,symbolIndex));
+        number2 = Number(tempstring.substring(symbolIndex+1,tempstring.length));
+        performMath(number1, number2 ,symbol);
+    }
+    else {
+      
+        let  number1 = Number(tempstring.substring(0,symbols[0]));
+        let symbolIndex1 = symbols[0];
+        let symbolIndex2;
+        for(let j=1; j <= symbols.length ; j++){
+            symbolIndex2 = symbols[j];
+            symbolIndex2 = symbols[j];
+            symbol = tempstring[symbolIndex1];
+            number2 = Number(tempstring.substring(symbolIndex1+1,symbolIndex2));
+            performMath(number1, number2 ,symbol);          
+            number1 = result;
+            symbolIndex1 = symbolIndex2;
+                
         }
     }
-    tempstring = result;
+    
+    tempstring=result;
 }
 
   
 function init() {
     document.querySelector(".calc-buttons").addEventListener("click", function(event) {
+
         if(event.target.innerText === "\u003D"){
             processTempString();
         } else if(event.target.innerText === "\u2190"){
@@ -60,7 +91,6 @@ function init() {
         }
         else {
             tempstring = tempstring + event.target.innerText;
-
         }
         getResult();
     });
